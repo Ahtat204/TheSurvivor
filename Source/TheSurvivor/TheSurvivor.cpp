20c957bdd09af3a26d0e8480accd52f876ade153
Lahcen <lahcen28ahtat@gmail.com>
2026-03-09 18:54:33 +0100

    Initial commit

---
+++ Source/TheSurvivor/TheSurvivor.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TheSurvivor.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TheSurvivor, "TheSurvivor" );
