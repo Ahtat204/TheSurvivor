ed45b4797f3d2f0451232582061b4d4837ccca34
Lahcen <lahcen28ahtat@gmail.com>
2026-03-16 10:04:50 +0100

    more character setup and functions implmentation

--- Source/TheSurvivor/Private/SurvivorCharacter.cpp
+++ Source/TheSurvivor/Private/SurvivorCharacter.cpp
@@ -1,16 +1,48 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "SurvivorCharacter.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
 
 ASurvivorCharacter::ASurvivorCharacter(const FObjectInitializer& ObjectInitialize)
 {
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = true;
+	bUseControllerRotationRoll = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f);
+	GetCharacterMovement()->JumpZVelocity = 700.f;
+	GetCharacterMovement()->AirControl = 0.35f;
+	GetCharacterMovement()->MaxWalkSpeed = 500.f;
+	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetMesh(),TEXT("head"));
+	CameraBoom->TargetArmLength = 400.0f; 
+	CameraBoom->bUsePawnControlRotation = true; 
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false; 
+	GetCharacterMovement()->bOrientRotationToMovement = false;
 }
+
 void ASurvivorCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if (auto const PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<
+			UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(FreeMappingContext, 0);
+		}
+	}
 }
 
 void ASurvivorCharacter::Reload(const FInputActionValue& Value)
@@ -20,36 +52,55 @@ void ASurvivorCharacter::Reload(const FInputActionValue& Value)
 void ASurvivorCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 void ASurvivorCharacter::Shoot(const FInputActionValue& Value)
 {
+	
 }
 
 void ASurvivorCharacter::Aim(const FInputActionValue& Value)
 {
 }
 
-void ASurvivorCharacter::DropWeapon(const FInputActionValue& Value)
-{
-}
-
 void ASurvivorCharacter::Move(const FInputActionValue& Value)
 {
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+	if (Controller != nullptr)
+	{
+		const auto Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+		const auto ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		const auto RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
 }
 
-void ASurvivorCharacter::PickUpPistol(const FInputActionValue& Value)
-{
-}
 
 void ASurvivorCharacter::Look(const FInputActionValue& Value)
 {
+	const auto LookAxisVector = Value.Get<FVector2D>();
+	if (Controller != nullptr)
+	{
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
 }
 
 void ASurvivorCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	if (auto EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ASurvivorCharacter::Move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ASurvivorCharacter::Look);
+		EnhancedInputComponent->BindAction(AimAction, ETriggerEvent::Triggered, this, &ASurvivorCharacter::Aim);
+		EnhancedInputComponent->BindAction(AimAction, ETriggerEvent::Completed, this, &ASurvivorCharacter::Aim);
+		EnhancedInputComponent->BindAction(FireAction, ETriggerEvent::Started, this, &ASurvivorCharacter::Shoot);
+		EnhancedInputComponent->BindAction(ReloadAction, ETriggerEvent::Started, this,&ASurvivorCharacter::Reload);
 
+	}
 }
 
--- Source/TheSurvivor/TheSurvivor.Build.cs
+++ Source/TheSurvivor/TheSurvivor.Build.cs
@@ -8,9 +8,9 @@ public class TheSurvivor : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "EnhancedInput" });
 		PublicIncludePaths.AddRange( new string[] {"TheSurvivor/Public/**"});
 
 		// Uncomment if you are using Slate UI
